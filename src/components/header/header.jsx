892884bdeda136d9af973bd1f743a03f52ea146d
LauraShy <78038876+LauraShy@users.noreply.github.com>
2021-09-29 13:40:51 -0400

    Try old nav with updated ''

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -8,7 +8,7 @@ const Header = (props) => {
     localStorage.clear();
     window.open('/', '_self');
   }
-  const { user, history } = props;
+  const { user } = props;
   console.log(user)
   return (
     <>
@@ -25,8 +25,8 @@ const Header = (props) => {
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link onClick={() => { history.push(`/`); }}>Movies</Nav.Link>
-          <Nav.Link onClick={() => { history.push(`/users/${user}`); }}>Profile</Nav.Link>
+          <Nav.Link href='/'>Movies</Nav.Link>
+          <Nav.Link href={'/users/${user}'}>Profile</Nav.Link>
           <Nav.Link onClick={() => { onLoggedOut() }}>Logout</Nav.Link>
         </Nav>
       </Navbar.Collapse>
